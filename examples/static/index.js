700bbcc29d785370d5dad2b078ce6e783aee1ee3
Andrew Gremlich <agremlich@anonyome.com>
2024-04-19 17:24:00 -0600

    testing

--- examples/static/index.js
+++ examples/static/index.js
@@ -1,12 +1,12 @@
 (async () => {
   const setCookie = await fetch('https://loud-cod-70.deno.dev/api/cookie', {
-    credentials: 'include',
+    mode: 'no-cors'
   })
 
   console.log(await setCookie.text())
 
   const readCookie = await fetch('https://loud-cod-70.deno.dev/api/read-cookie', {
-    credentials: 'include',
+    mode: 'no-cors'
   })
 
   console.log(await readCookie.text())
