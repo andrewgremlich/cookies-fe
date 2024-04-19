4e51bb9591c0fccdd0ede5e880a30c783f4f94cb
Andrew Gremlich <agremlich@anonyome.com>
2024-04-19 16:30:47 -0600

    changing api host

--- examples/static/index.js
+++ examples/static/index.js
@@ -1,12 +1,12 @@
 (async () => {
-  const setCookie = await fetch('http://localhost:8000/api/cookie', {
+  const setCookie = await fetch('https://loud-cod-70.deno.dev/api/cookie', {
     withCredentials: true,
     mode: 'cors'
   })
 
   console.log(await setCookie.text())
 
-  const readCookie = await fetch('http://localhost:8000/api/read-cookie', {
+  const readCookie = await fetch('https://loud-cod-70.deno.dev/api/read-cookie', {
     withCredentials: true,
     mode: 'cors'
   })
